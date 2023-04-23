1dca2576c990b8c0dc91239d1a562f09bc42aa9c
Kim Gwang woon <misili304@naver.com>
2023-04-23 16:42:50 +0900

    Feat/ Comment edit, delete

--- articles/views.py
+++ articles/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from users.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework import status
-from articles.models import Article
+from articles.models import Article, Comment
 from articles.serializers import (
     ArticleSerializer,
     ArticleListSerializer,
@@ -73,11 +73,25 @@ class CommentView(APIView):
 
 
 class CommentDetailView(APIView):
-    def put(self, request, article_id):
-        pass
+    def put(self, request, article_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다", status=status.HTTP_403_FORBIDDEN)
 
-    def delete(self, request, article_id):
-        pass
+    def delete(self, request, article_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다", status=status.HTTP_403_FORBIDDEN)
 
 
 class LikeView(APIView):
